ac44eb954f1ca06a775492710c002fcf7f9e4c68
Jean-Claude Passy <jean-claude.passy@tuebingen.mpg.de>
2020-12-28 23:03:48 +0100

    Same for group 2.

--- test_and_doc/group2/sample_project/math.py
+++ test_and_doc/group2/sample_project/math.py
@@ -5,3 +5,17 @@ class Sequence:
 
     def arithmetic_mean(self):
         return sum(self.data) / len(self.data)
+
+    def geometric_mean(self):
+
+        n = len(self.data)
+
+        if not n:
+            raise ValueError("There should be at least one element in the data.")
+
+        assert n > 0, "Bad bad..."
+
+        product = 1
+        for e in self.data:
+            product *= e
+        return product ** (1 / n)
--- test_and_doc/group2/tests/test_math.py
+++ test_and_doc/group2/tests/test_math.py
@@ -1,5 +1,41 @@
+import random
 from unittest import TestCase
+from time import time
+
+from sample_project.math import Sequence
 
 
 class TestGeomean(TestCase):
-    pass
+
+    def setUp(self):
+        seed = time()
+        random.seed(seed)
+        print(f"The seed for the RNG is {seed}")
+
+    def tearDown(self):
+        pass
+
+    def test_toto(self):
+        pass
+
+    def test_geomean_basic(self):
+
+        x = Sequence([7])
+        self.assertEqual(x.geometric_mean(), 7)
+
+        y = Sequence([7, 9, 1])
+        self.assertEqual(y.geometric_mean(), 63 ** (1 / 3))
+
+    def test_random(self):
+
+        num_elems = random.randrange(10) + 1
+        print("num_elems = ", num_elems)
+        vals = []
+        for _ in range(num_elems):
+            vals.append(random.randrange(10))
+
+        x = Sequence(vals)
+        print("vals = ", vals)
+        g_mean = x.geometric_mean()
+        a_mean = x.arithmetic_mean()
+        self.assertLessEqual(g_mean, a_mean)
